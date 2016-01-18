02fad30763526787b75d7c30589bde610613c79a
boo1ean <boo1ean0807@gmail.com>
2016-01-19 01:05:18 +0200

    Update github events handlers to pick repository-specific config

--- src/server.js
+++ src/server.js
@@ -1,23 +1,46 @@
+var path = require('path');
+var fs = require('fs');
 var githubEvents = require('github-events');
 var startContainer = require('./start-container');
 var updateContainer = require('./update-container');
 var destroyContainer = require('./destroy-container');
-var path = require('path');
 
 var events = githubEvents({ port: 3333 });
 var workspacePath = path.resolve(__dirname + '/../workspace');
+var config = JSON.parse(fs.readFileSync(__dirname + '/../config.json').toString());
+
+console.log('started with config:');
+console.log(JSON.stringify(config, null, 4));
 
 events.on('branch created', (branchName, params) => {
-	console.log('new branch created: %s', branchName);
-	startContainer(workspacePath, branchName);
+	var repo = params.repository.full_name;
+	var deployConfig = config.repos[repo];
+	if (!deployConfig) {
+		return console.error('repo config not found');
+	}
+
+	console.log('new branch created for %s %s', repo, branchName);
+	startContainer(workspacePath, branchName, deployConfig);
 });
 
 events.on('commits pushed', (branchName, params) => {
-	console.log('update container: %s', branchName);
-	updateContainer(workspacePath, branchName);
+	var repo = params.repository.full_name;
+	var deployConfig = config.repos[repo];
+	if (!deployConfig) {
+		return console.error('repo config not found');
+	}
+
+	console.log('update container for %s %s', repo, branchName);
+	updateContainer(workspacePath, branchName, deployConfig);
 });
 
 events.on('branch deleted', (branchName, params) => {
-	console.log('destroy container: %s', branchName);
-	destroyContainer(workspacePath, branchName);
+	var repo = params.repository.full_name;
+	var deployConfig = config.repos[repo];
+	if (!deployConfig) {
+		return console.error('repo config not found');
+	}
+
+	console.log('destroy container for %s %s', repo, branchName);
+	destroyContainer(workspacePath, branchName, deployConfig);
 });
